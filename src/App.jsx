4e16e93391f6df41fbbf106243ff938a2555d7d8
sheth chirag <102785098+shethchirag@users.noreply.github.com>
2024-04-25 15:17:29 +0530

    frontend part end

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import MyState from "./context/data/myState";
 import Login from "./pages/registration/Login";
 import Signup from "./pages/registration/Signup";
 import ProductInfo from "./pages/productInfo/ProductInfo";
+import UpdateProduct from "./pages/admin/pages/UpdateProduct";
+import AddProduct from "./pages/admin/pages/AddProduct";
 
 const App = () => {
   return (
@@ -22,6 +24,8 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/productinfo/:id" element={<ProductInfo />} />
+          <Route path="/updateproduct" element={<UpdateProduct />} />
+          <Route path="/addproduct" element={<AddProduct />} />
           <Route path="/*" element={<NoPage />} />
         </Routes>
       </BrowserRouter>
